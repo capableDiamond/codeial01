5444141eb7b47d1b70250ac7c569ad3368263712
Shivam Aggarwal <shivamaggarwal.youngengine@gmail.com>
2023-03-09 18:40:37 +0530

    add views engine ejs to the project

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = 8000;
 //require the index of routes, it is via this file all other routes are made accessible
 app.use('/',require('./routes'));
 
+//setting up view engine ejs
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,(err) =>{
     if(err){
         console.log(`Error in running the server: ${err}`);
