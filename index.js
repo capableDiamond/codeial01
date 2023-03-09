2a8adac37c9521aa146f1f8c2b1e04c69bad7707
Shivam Aggarwal <shivamaggarwal.youngengine@gmail.com>
2023-03-09 19:56:25 +0530

    Implementing Layouts

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 //use express router
 //require the index of routes, it is via this file all other routes are made accessible
