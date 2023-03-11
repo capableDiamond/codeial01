4e6edbb3d14706816e4f4b12e7ff20cd6f3d2a74
Shivam Aggarwal <shivamaggarwal.youngengine@gmail.com>
2023-03-11 12:14:26 +0530

    Create User Sign Up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,55 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req,res){
     return res.render('users',{
         title:'users'
     });
 
     // res.end('<h1>User Profile</h1>');
+}
+
+//renders the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:'Codeial | Sign Up'
+    });
+}
+
+//renders the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title:'Codeial | Sign zin'
+    });
+}
+
+//Get the sign up data
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email:req.body.email},function(error,user){
+        if(error){
+            console.log('Error in finding user in signing up');
+            return;
+        }
+
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(error){
+                    console.log('Error in creating user while signing up');
+                    return;
+                }
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
+
+//sign in and create a session for the user
+module.exports.createSession = function(req,res){
+    //TODO Later
 }
