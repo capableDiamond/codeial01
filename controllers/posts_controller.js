6ed47249aaef62376970e45217beda500e353df3
Shivam Aggarwal <shivamaggarwal.youngengine@gmail.com>
2023-03-12 20:39:47 +0530

    Save posts by user to db

---
+++ controllers/posts_controller.js
@@ -0,0 +1,11 @@
+const Post = require('../models/post');
+
+module.exports.create = function(req,res){
+    Post.create({
+        content:req.body.content,
+        user:req.user._id// this was set by the set authenticated user function created in passport local strategy in config
+    },function(err,post){
+        if(err){console.log('Error in creating a post');return;}
+        return res.redirect('back');
+    });
+}
