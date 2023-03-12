15f05ca52d6f714a9fbb47c25b2018e56d397778
Shivam Aggarwal <shivamaggarwal.youngengine@gmail.com>
2023-03-12 23:18:35 +0530

    Display Posts and related Users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,24 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    // return res.end('<h1>Express is up for Codeial!</h1>');
-    return res.render('home',{
-        title:"Home"
+    // Post.find({},function(err,posts){
+    //     if(err){console.log('Error in fetching posts from db');return;}
+
+    //     return res.render('home',{
+    //         title:"Codeial | Home",
+    //         posts:posts
+    //     });
+    // });
+
+    //first we find all the posts and then we populate alt eh users in the posts and then we call the callback function
+    Post.find({}).populate('user').exec(function(err,posts){
+        if(err){console.log('Error in fetching posts from db');return;}
+
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts:posts
+        });
     });
+
+
 }
