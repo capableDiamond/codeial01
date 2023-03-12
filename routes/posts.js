f62c8e535e05833a2b18badd6a8958f3a2f2ee45
Shivam Aggarwal <shivamaggarwal.youngengine@gmail.com>
2023-03-12 23:34:06 +0530

    Check Authentication on Creating Posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
 
 const postController = require('../controllers/posts_controller');
-
-router.post('/create',postController.create);
+//checks if the user is signed in before allowing him to make a post
+router.post('/create',passport.checkAuthentication,postController.create);
 
 module.exports = router;
