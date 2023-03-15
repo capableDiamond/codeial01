a7fac6ef0e57a6a7d8e5687074391e2467cdfa87
Shivam Aggarwal <shivamaggarwal.youngengine@gmail.com>
2023-03-15 13:43:07 +0530

    Deleting a Comment(Authorized)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -20,3 +20,27 @@ module.exports.create = function(req,res){
         }
     });
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(err){console.log(err);return;}
+        //TODO learn to implement asycn with help of promises
+        let postId = comment.post;
+        let postOwner;
+        Post.findById(postId,function(err,post){
+            postOwner = post.user.toString();
+
+            if(comment.user == req.user.id || req.user.id == postOwner){
+                comment.remove();
+
+                post.update({$pull: {comments:req.params.id}},function(err,post){
+                    return res.redirect('back');
+            });
+            }else{
+                console.log('User not allowed to delete comment');
+                return res.redirect('back');
+            }
+
+        })
+    })
+}
--- routes/comments.js
+++ routes/comments.js
@@ -6,5 +6,6 @@ const passport = require('passport');
 const commentsController = require('../controllers/comments_controller');
 //checks if the user is signed in before allowing him to make a comment
 router.post('/create',passport.checkAuthentication,commentsController.create);
+router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
 
 module.exports = router;
