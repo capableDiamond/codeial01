ff33e1839b0be4bb0e2f97c237f230e9b47140b8
Shivam Aggarwal <shivamaggarwal.youngengine@gmail.com>
2023-03-15 19:40:36 +0530

    Updating a User Profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,30 +7,30 @@ module.exports.profile = function(req,res){
             profile_user:user
         });
     });
-    
-
 }
 
 //renders the sign up page
 module.exports.signUp = function(req,res){
-    if(req.isAuthenticated()){
-       return res.redirect('/users/profile');
+    if(req.isAuthenticated()){ //this prevents signed in users from accessing this page while they are signed in
+       return res.redirect('/');
+    }else{
+        return res.render('user_sign_up',{
+            title:'Codeial | Sign Up'
+        });
     }
-
-    return res.render('user_sign_up',{
-        title:'Codeial | Sign Up'
-    });
 }
 
 //renders the sign in page
 module.exports.signIn = function(req,res){
-    if(req.isAuthenticated()){
-        return res.redirect('/users/profile');
+    //this function is call to the sign in page
+    if(req.isAuthenticated()){ //this prevents signed in users from accessing this page while they are signed in
+        return res.redirect('/');
     }
-
-    return res.render('user_sign_in',{
-        title:'Codeial | Sign in'
-    });
+    else{
+        return res.render('user_sign_in',{
+            title:'Codeial | Sign in'
+        });
+    }
 }
 
 //Get the sign up data
@@ -62,12 +62,23 @@ module.exports.create = function(req,res){
 
 //sign in and create a session for the user
 module.exports.createSession = function(req,res){
-    return res.redirect('/users/profile');
+    return res.redirect('/');
 }
 
 module.exports.destroySession = function(req,res){
-    req.logout(function(err){
+    req.logout(function(err){//logout is a function of passport library
         if(err){console.log('Error in Logging out');return;}
         res.redirect('/');
     });
+}
+
+module.exports.update = function(req,res){
+    //to check if the info of the logged in user is being changed and not of other user
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
 }
